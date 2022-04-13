63f2662cf8c36927a41727f5bb824fd71528f433
Sean Carey <wycleffsean@gmail.com>
2022-04-13 08:39:55 -0400

    lexer: include location information with tokens

---
+++ README.md
@@ -0,0 +1,3 @@
+## Testing
+
+`python3 test.py`
--- src/lex.py
+++ src/lex.py
@@ -20,8 +20,10 @@ class LexError(Exception):
 class EOFException(Exception):
     pass
 
-class Lex:
+class Lexer:
     curr = 0
+    line = 1 # 1 indexed, latent increment
+    col = 0 # 1 indexed, immediate increment
 
     def __init__(self, buf: str):
         self.buf = buf
@@ -29,14 +31,18 @@ class Lex:
         if self.curr == len(self.buf):
             raise EOFException
         return self.buf[self.curr]
-    def __peek(self):
-        if self.curr == len(self.buf):
-            raise EOFException
-        return self.buf[self.curr]
     def __prior(self):
-        return self.buf[self.curr - 1]
+        if self.curr > 0:
+            return self.buf[self.curr - 1]
+        else:
+            return ''
     def __take(self):
         val = self.__peek()
+        if self.__prior() == '\n':
+            self.line += 1
+            self.col = 0
+        else:
+            self.col += 1
         self.curr += 1
         return val
     # needle must be a single char
@@ -44,28 +50,32 @@ class Lex:
         val = io.StringIO()
         while self.__peek() != '"' or self.__prior() == '\\':
             val.write(self.__take())
+        self.__take() # throwaway terminal quote
         return val.getvalue()
+    def __location(self) -> (int, int):
+        return (self.line, self.col)
 
     def next(self):
-        #if self.curr == len(self.string):
-
         try:
             val = self.__take()
             if val == '(':
-                return (Token.LPAREN)
+                return (Token.LPAREN, self.__location())
             elif val == ')':
-                return (Token.RPAREN)
+                return (Token.RPAREN, self.__location())
             elif val == ' ':
-                return (Token.SPACE)
+                return (Token.SPACE, self.__location())
             elif val == '\t':
-                return (Token.INDENT)
+                return (Token.INDENT, self.__location())
             elif val == '\n':
-                return (Token.NEWLINE)
+                return (Token.NEWLINE, self.__location())
             elif val == ':':
-                return (Token.COLON)
+                return (Token.COLON, self.__location())
             elif val == '"':
-                return (Token.STRING, self.__take_string())
+                begin = self.__location()
+                string = self.__take_string()
+                return (Token.STRING, begin, self.__location(), string)
             elif val >= 'A' and val <= 'z':
+                begin = self.__location()
                 ret_val = io.StringIO()
                 ret_val.write(val)
                 next_val = val
@@ -75,58 +85,58 @@ class Lex:
                         next_val = self.__peek()
                     except EOFException:
                         break
-                return (Token.SYMBOL, ret_val.getvalue())
+                return (Token.SYMBOL, begin, self.__location(), ret_val.getvalue())
             else:
                 raise LexError
         except EOFException:
-            return (Token.EOF)
+            return (Token.EOF, self.__location())
 
-class TestLex(unittest.TestCase):
+class TestLexer(unittest.TestCase):
     def test__take(self):
-        lexer = Lex("F")
+        lexer = Lexer("F")
         self.assertEqual(lexer.curr, 0)
-        self.assertEqual(lexer._Lex__take(), 'F') # unmangling this will suck
+        self.assertEqual(lexer._Lexer__take(), 'F') # unmangling this will suck
         self.assertEqual(lexer.curr, 1)
 
     def test__peek(self):
-        lexer = Lex("F")
+        lexer = Lexer("F")
         self.assertEqual(lexer.curr, 0)
-        self.assertEqual(lexer._Lex__peek(), 'F')
+        self.assertEqual(lexer._Lexer__peek(), 'F')
         self.assertEqual(lexer.curr, 0)
 
     def test_parens(self):
-        lexer = Lex("()")
-        self.assertEqual(lexer.next(), (Token.LPAREN))
-        self.assertEqual(lexer.next(), (Token.RPAREN))
-        self.assertEqual(lexer.next(), (Token.EOF))
+        lexer = Lexer("()")
+        self.assertEqual(lexer.next(), (Token.LPAREN, (1, 1)))
+        self.assertEqual(lexer.next(), (Token.RPAREN, (1, 2)))
+        self.assertEqual(lexer.next(), (Token.EOF, (1, 2)))
 
     def test_colon(self):
-        lexer = Lex(":")
-        self.assertEqual(lexer.next(), (Token.COLON))
-        self.assertEqual(lexer.next(), (Token.EOF))
+        lexer = Lexer(":")
+        self.assertEqual(lexer.next(), (Token.COLON, (1, 1)))
+        self.assertEqual(lexer.next(), (Token.EOF, (1, 1)))
 
     def test_whitespace(self):
-        lexer = Lex(" \t\n")
-        self.assertEqual(lexer.next(), (Token.SPACE))
-        self.assertEqual(lexer.next(), (Token.INDENT))
-        self.assertEqual(lexer.next(), (Token.NEWLINE))
-        self.assertEqual(lexer.next(), (Token.EOF))
+        lexer = Lexer(" \t\n")
+        self.assertEqual(lexer.next(), (Token.SPACE, (1, 1)))
+        self.assertEqual(lexer.next(), (Token.INDENT, (1, 2)))
+        self.assertEqual(lexer.next(), (Token.NEWLINE, (1, 3)))
+        self.assertEqual(lexer.next(), (Token.EOF, (1, 3)))
 
     def test_symbol(self):
-        lexer = Lex("thing ")
-        self.assertEqual(lexer.next(), (Token.SYMBOL, 'thing'))
-        self.assertEqual(lexer.next(), (Token.SPACE))
-        self.assertEqual(lexer.next(), (Token.EOF))
+        lexer = Lexer("thing ")
+        self.assertEqual(lexer.next(), (Token.SYMBOL, (1, 1), (1, 5), 'thing'))
+        self.assertEqual(lexer.next(), (Token.SPACE, (1, 6)))
+        self.assertEqual(lexer.next(), (Token.EOF, (1, 6)))
 
     def test_symbol_terminal(self):
-        lexer = Lex("thing")
-        self.assertEqual(lexer.next(), (Token.SYMBOL, 'thing'))
-        self.assertEqual(lexer.next(), (Token.EOF))
+        lexer = Lexer("thing")
+        self.assertEqual(lexer.next(), (Token.SYMBOL, (1, 1), (1, 5), 'thing'))
+        self.assertEqual(lexer.next(), (Token.EOF, (1, 5)))
 
     def test_string(self):
-        lexer = Lex("\"thing\"")
-        self.assertEqual(lexer.next(), (Token.STRING, 'thing'))
-        self.assertEqual(lexer.next(), (Token.EOF))
+        lexer = Lexer("\"thing\"")
+        self.assertEqual(lexer.next(), (Token.STRING, (1, 1), (1, 7), 'thing'))
+        self.assertEqual(lexer.next(), (Token.EOF, (1, 7)))
 
 if __name__ == '__main__':
     unittest.main()
