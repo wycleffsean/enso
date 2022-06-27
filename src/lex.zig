e9233b31647fcff4c8bf9740b9293c618b15d88d
Sean Carey <seanpcarey@protonmail.com>
2022-06-26 23:35:19 -0400

    wip2: zig lexer

--- src/lex.zig
+++ src/lex.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const bufferedReader = std.io.bufferedReader;
-const fixedBufferStream = std.io.fixedBufferStream;
+const StreamSource = std.io.StreamSource;
+
 const testing = std.testing;
+const fixedBufferStream = std.io.fixedBufferStream;
 
 const LineLength = u32;
 const ColLength = u32;
@@ -52,111 +54,126 @@ const Token = union(TokenTag) {
     RCBRACKET: Location,
 };
 
-pub fn Lexer(comptime ReaderType: type) type {
-    return struct {
-        reader: ReaderType,
-        curr: ?u8 = null,
-        prior: ?u8 = null,
-        col: ColLength = 0,
-        line: LineLength = 1, // 1 indexed, latent increment col = 0 # 1 indexed, immediate increment
-        indent: u32 = 0, // 0 indexed, immediate increment
-        const Self = @This();
+const Lexer = struct {
+    reader: std.io.BufferedReader(4096, StreamSource.Reader).Reader,
+    curr: ?u8 = null,
+    prior: ?u8 = null,
+    col: ColLength = 0,
+    line: LineLength = 1, // 1 indexed, latent increment col = 0 # 1 indexed, immediate increment
+    indent: u32 = 0, // 0 indexed, immediate increment
+    internbuf: [16384]u8 = undefined,
+    const Self = @This();
 
-        const Error = error{
-            BadToken,
-            EOF,
-        };
-
-        fn peek(self: *Self) Error!u8 {
-            if (self.curr) |curr| {
-                return curr;
-            }
-
-            const byte = self.reader.readByte() catch return Error.EOF;
-            self.curr = byte;
-            return byte;
-        }
-
-        fn is(val: ?u8, comptime lit: u8) bool {
-            if (val) |value| {
-                return value == lit;
-            }
-            return false;
-        }
-
-        fn take(self: *Self) Error!u8 {
-            var byte = try self.peek();
-            if (is(self.prior, '\n')) {
-                self.line += 1;
-                self.col = 0;
-            } else {
-                self.col += 1;
-            }
-            self.prior = self.curr;
-            self.curr = null;
-            return byte;
-        }
-
-        fn location(self: Self) Location {
-            return .{ .line = self.line, .col = self.col };
-        }
-
-        fn readChars(self: *Self) Error![]const u8 {
-            while(true) {
-                switch(self.peek
-            }
-        }
-
-        pub fn next(self: *Self) Error!Token {
-            const byte = self.take() catch return Token{ .EOF = self.location() };
-            switch (byte) {
-                // whitespace
-                '\n', '\t', ' ' => return self.next(),
-                // brackets and operators
-                '(' => return Token{ .LPAREN = self.location() },
-                ')' => return Token{ .RPAREN = self.location() },
-                ':' => return Token{ .COLON = self.location() },
-                ',' => return Token{ .COMMA = self.location() },
-                '+' => return Token{ .PLUS = self.location() },
-                '-' => return Token{ .MINUS = self.location() },
-                '*' => return Token{ .ASTERISK = self.location() },
-                '/' => return Token{ .SOLIDUS = self.location() },
-                '<' => return Token{ .LESS = self.location() },
-                '>' => return Token{ .GREATER = self.location() },
-                '=' => return Token{ .EQUAL = self.location() },
-                '!' => return Token{ .BANG = self.location() },
-                '[' => return Token{ .LSBRACKET = self.location() },
-                ']' => return Token{ .RSBRACKET = self.location() },
-                '{' => return Token{ .LCBRACKET = self.location() },
-                '}' => return Token{ .RCBRACKET = self.location() },
-                'A'...'z' => {
-                    const begin = self.location();
-                    const name = self.readChars();
-                    return Token{ .SYMBOL = .{ .name = name, .begin = begin, .end = self.location } };
-                },
-                else => return Error.BadToken,
-            }
-        }
+    const Error = error{
+        BadToken,
+        EOF,
     };
-}
 
-pub fn lexer(reader: anytype) Lexer(@TypeOf(reader)) {
-    return .{ .reader = reader };
-}
+    fn peek(self: *Self) Error!u8 {
+        if (self.curr) |curr| {
+            return curr;
+        }
+
+        const byte = self.reader.readByte() catch return Error.EOF;
+        self.curr = byte;
+        return byte;
+    }
+
+    fn is(val: ?u8, comptime lit: u8) bool {
+        if (val) |value| {
+            return value == lit;
+        }
+        return false;
+    }
+
+    fn take(self: *Self) Error!u8 {
+        var byte = try self.peek();
+        if (is(self.prior, '\n')) {
+            self.line += 1;
+            self.col = 0;
+        } else {
+            self.col += 1;
+        }
+        self.prior = self.curr;
+        self.curr = null;
+        return byte;
+    }
+
+    fn location(self: Self) Location {
+        return .{ .line = self.line, .col = self.col };
+    }
+
+    fn readChars(self: *Self, buf: []const u8) Error!void {
+        var i: u8 = 1;
+        while (i < buf.len) : (i += 1) {
+            const byte = try self.peek();
+            switch (byte) {
+                'A'...'Z', 'a'...'z' => {
+                    buf[i] = try self.take();
+                },
+                else => return,
+            }
+        }
+    }
+
+    pub fn next(self: *Self) Error!Token {
+        const byte = self.take() catch return Token{ .EOF = self.location() };
+        switch (byte) {
+            // whitespace
+            '\n', '\t', ' ' => return self.next(),
+            // brackets and operators
+            '(' => return Token{ .LPAREN = self.location() },
+            ')' => return Token{ .RPAREN = self.location() },
+            ':' => return Token{ .COLON = self.location() },
+            ',' => return Token{ .COMMA = self.location() },
+            '+' => return Token{ .PLUS = self.location() },
+            '-' => return Token{ .MINUS = self.location() },
+            '*' => return Token{ .ASTERISK = self.location() },
+            '/' => return Token{ .SOLIDUS = self.location() },
+            '<' => return Token{ .LESS = self.location() },
+            '>' => return Token{ .GREATER = self.location() },
+            '=' => return Token{ .EQUAL = self.location() },
+            '!' => return Token{ .BANG = self.location() },
+            '[' => return Token{ .LSBRACKET = self.location() },
+            ']' => return Token{ .RSBRACKET = self.location() },
+            '{' => return Token{ .LCBRACKET = self.location() },
+            '}' => return Token{ .RCBRACKET = self.location() },
+            'A'...'Z', 'a'...'z' => {
+                const begin = self.location();
+                var tok = Token{ .SYMBOL = .{ .name = undefined, .begin = begin, .end = self.location() } };
+                tok.SYMBOL.name[0] = byte;
+                try self.readChars(&tok.SYMBOL.name);
+                return tok;
+            },
+            else => return Error.BadToken,
+        }
+    }
+};
+
+//pub fn lexer(stream: *StreamSource) Lexer {
+//    return .{ .reader = bufferedReader(stream.reader()).reader() };
+//}
+
+//fn testStream(buf: []const u8) StreamSource {
+//    return StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+//}
 
 test "peek" {
-    var reader = fixedBufferStream("F"[0..]).reader();
-    var lex = lexer(reader);
+    const buf = "F";
+    var stream = StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+    var lex = Lexer{ .reader = bufferedReader(stream.reader()).reader() };
     try testing.expectEqual(lex.curr, null);
     try testing.expectEqual(lex.col, 0);
-    try testing.expectEqual(lex.peek(), 'F');
+    const x = lex.peek();
+    try testing.expectEqual(x, 'F');
     try testing.expectEqual(lex.curr, 'F');
     try testing.expectEqual(lex.col, 0);
 }
 
 test "take" {
-    var reader = fixedBufferStream("F"[0..]).reader();
-    var lex = lexer(reader);
+    const buf = "F";
+    var stream = StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+    var lex = Lexer{ .reader = bufferedReader(stream.reader()).reader() };
     try testing.expectEqual(lex.col, 0);
     try testing.expectEqual(lex.take(), 'F');
     try testing.expectEqual(lex.curr, null);
@@ -165,15 +182,17 @@ test "take" {
 }
 
 test "parens" {
-    var reader = fixedBufferStream("()"[0..]).reader();
-    var lex = lexer(reader);
+    const buf = "()";
+    var stream = StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+    var lex = Lexer{ .reader = bufferedReader(stream.reader()).reader() };
     try testing.expectEqual(lex.next(), .{ .LPAREN = .{ .line = 1, .col = 1 } });
     try testing.expectEqual(lex.next(), .{ .RPAREN = .{ .line = 1, .col = 2 } });
 }
 
 test "operators" {
-    var reader = fixedBufferStream(":,+-*/<>=![]{}"[0..]).reader();
-    var lex = lexer(reader);
+    const buf = ":,+-*/<>=![]{}";
+    var stream = StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+    var lex = Lexer{ .reader = bufferedReader(stream.reader()).reader() };
     try testing.expectEqual(lex.next(), .{ .COLON = .{ .line = 1, .col = 1 } });
     try testing.expectEqual(lex.next(), .{ .COMMA = .{ .line = 1, .col = 2 } });
     try testing.expectEqual(lex.next(), .{ .PLUS = .{ .line = 1, .col = 3 } });
@@ -191,13 +210,17 @@ test "operators" {
 }
 
 test "whitespace ignored" {
-    var reader = fixedBufferStream(" \t\n"[0..]).reader();
-    var lex = lexer(reader);
+    const buf = " \t\n";
+    var stream = StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+    var lex = Lexer{ .reader = bufferedReader(stream.reader()).reader() };
     try testing.expectEqual(lex.next(), .{ .EOF = .{ .line = 1, .col = 3 } });
 }
 
 test "symbol" {
-    var reader = fixedBufferStream("thing "[0..]).reader();
-    var lex = lexer(reader);
-    try testing.expectEqual(lex.next(), .{ .SYMBOL = .{ .name = "thing", .begin = .{ .line = 1, .col = 1 }, .end = .{ .line = 1, .col = 5 } } });
+    const buf = "thing ";
+    var stream = StreamSource{ .const_buffer = fixedBufferStream(buf[0..]) };
+    var lex = Lexer{ .reader = bufferedReader(stream.reader()).reader() };
+    // TODO
+    var name = "thing";
+    try testing.expectEqual(lex.next(), .{ .SYMBOL = .{ .name = name, .begin = .{ .line = 1, .col = 1 }, .end = .{ .line = 1, .col = 5 } } });
 }
