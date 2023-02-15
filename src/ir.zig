ac2d95eeca69532764dcdf459636ad34d843001a
Sean Carey <seanpcarey@protonmail.com>
2023-02-15 01:16:51 -0500

    ir: add concept of blocks and yield
    all blocks conclude with a yield

--- src/ir.zig
+++ src/ir.zig
@@ -15,6 +15,7 @@ const InsnType = enum {
     product,
     division,
     call,
+    yield, // ie return
 };
 
 pub const Insn = union(InsnType) {
@@ -27,12 +28,22 @@ pub const Insn = union(InsnType) {
     product: void,
     division: void,
     call: void,
+    yield: void,
+};
+
+const Block = struct {
+    parent: ?*const Block,
+};
+
+const StackItem = union(enum) {
+    ast_node: *const AstNode,
+    block: Block,
+    block_end: Block,
 };
 
 pub const IrGen = struct {
     ast: *const AstNode,
-    list: std.ArrayList(Insn),
-    stack: std.ArrayList(*const AstNode),
+    stack: std.ArrayList(StackItem),
     intern_pool: *intern.StringInternPool,
 
     const Self = @This();
@@ -41,17 +52,14 @@ pub const IrGen = struct {
 
     // storing the arena on the struct leads to a segfault for some reason
     pub fn init(
-        allocator: std.mem.Allocator,
         arena: *std.heap.ArenaAllocator,
         intern_pool: *intern.StringInternPool,
         ast: *const AstNode,
     ) Self {
-        var list = std.ArrayList(Insn).init(allocator);
-        var stack = std.ArrayList(*const AstNode).init(arena.allocator());
+        var stack = std.ArrayList(StackItem).init(arena.allocator());
         return .{
             .ast = ast,
             .intern_pool = intern_pool,
-            .list = list,
             .stack = stack,
         };
     }
@@ -60,35 +68,39 @@ pub const IrGen = struct {
         self.stack.deinit();
     }
 
-    fn buildStack(self: *Self, ast: *const AstNode) Error!void {
+    fn buildStack(self: *Self, ast: *const AstNode, block: *const Block) Error!void {
         switch (ast.*) {
             .sum, .product, .division, .assignment => |node| {
                 // @call(.{ .always_tail }, buildStack, .{self, ast
-                try self.buildStack(node.lhs);
-                try self.buildStack(node.rhs);
-                try self.stack.append(ast);
+                try self.buildStack(node.lhs, block);
+                try self.buildStack(node.rhs, block);
+                try self.stack.append(.{ .ast_node = ast });
             },
             .integer => |_| {
-                try self.stack.append(ast);
+                try self.stack.append(.{ .ast_node = ast });
             },
             .group => |group| {
-                try self.buildStack(group.value);
+                try self.buildStack(group.value, block);
             },
             .name => |_| {
-                try self.stack.append(ast);
+                try self.stack.append(.{ .ast_node = ast });
             },
             .var_decl => |_| {
-                try self.stack.append(ast);
+                try self.stack.append(.{ .ast_node = ast });
             },
             .fn_decl => |fn_decl| {
-                try self.stack.append(ast);
+                try self.stack.append(.{ .block = Block{ .parent = block } });
+                // this is sketchy, but we need the stable pointer to the block
+                const fn_block = &self.stack.items[self.stack.items.len - 1].block;
+                try self.stack.append(.{ .ast_node = ast });
                 for (fn_decl.statement.items) |expr| {
-                    try self.buildStack(expr);
+                    try self.buildStack(expr, fn_block);
                 }
+                try self.stack.append(.{ .block_end = Block{ .parent = block } });
             },
             .call => |call| {
-                try self.stack.append(call.ref);
-                try self.stack.append(ast);
+                try self.stack.append(.{ .ast_node = call.ref });
+                try self.stack.append(.{ .ast_node = ast });
             },
         }
     }
@@ -111,20 +123,34 @@ pub const IrGen = struct {
         return insn;
     }
 
-    pub fn generate(self: *Self) Error!std.ArrayList(Insn) {
-        try self.buildStack(self.ast);
-        for (self.stack.items) |ast_node| {
-            const insn = try self.generateInsn(ast_node);
-            try self.list.append(insn);
+    pub fn generate(self: *Self, allocator: std.mem.Allocator) Error![]Insn {
+        var insns = std.ArrayList(Insn).init(allocator);
+        const root_block = Block{ .parent = null };
+        var current_block: *const Block = &root_block;
+        try self.buildStack(self.ast, current_block);
+        for (self.stack.items) |item| {
+            switch (item) {
+                .ast_node => |ast_node| {
+                    const insn = try self.generateInsn(ast_node);
+                    try insns.append(insn);
+                },
+                .block => |*block| {
+                    current_block = block;
+                },
+                .block_end => {
+                    try insns.append(Insn{ .yield = {} });
+                },
+            }
         }
-        return self.list;
+        try insns.append(Insn{ .yield = {} });
+        return insns.toOwnedSlice();
     }
 };
 
 const TestContext = struct {
     arena: *std.heap.ArenaAllocator,
     irgen: IrGen,
-    ir: std.ArrayList(Insn),
+    ir: []const Insn,
     intern_pool: *intern.StringInternPool,
 };
 
@@ -138,8 +164,8 @@ fn testSetup(code: []const u8) !TestContext {
 
     var intern_pool = try testing.allocator.create(intern.StringInternPool);
     intern_pool.* = intern.StringInternPool.init(arena.allocator());
-    var irgen = IrGen.init(testing.allocator, arena, intern_pool, ast);
-    var ir = try irgen.generate();
+    var irgen = IrGen.init(arena, intern_pool, ast);
+    var ir = try irgen.generate(testing.allocator);
     return TestContext{
         .arena = arena,
         .irgen = irgen,
@@ -152,7 +178,7 @@ fn testTeardown(ctx: *TestContext) void {
     ctx.irgen.deinit();
     ctx.intern_pool.deinit();
     ctx.arena.deinit();
-    ctx.ir.deinit();
+    testing.allocator.free(ctx.ir);
     testing.allocator.destroy(ctx.intern_pool);
     testing.allocator.destroy(ctx.arena);
 }
@@ -167,8 +193,9 @@ test "push integer" {
 
     var expected = [_]Insn{
         Insn{ .push_integer = .{ .value = 1 } },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
 
 test "ir sum" {
@@ -179,8 +206,9 @@ test "ir sum" {
         Insn{ .push_integer = .{ .value = 1 } },
         Insn{ .push_integer = .{ .value = 2 } },
         Insn{ .sum = {} },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
 
 test "ir product" {
@@ -191,8 +219,9 @@ test "ir product" {
         Insn{ .push_integer = .{ .value = 1 } },
         Insn{ .push_integer = .{ .value = 2 } },
         Insn{ .product = {} },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
 
 test "ir division" {
@@ -203,8 +232,9 @@ test "ir division" {
         Insn{ .push_integer = .{ .value = 1 } },
         Insn{ .push_integer = .{ .value = 2 } },
         Insn{ .division = {} },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
 
 test "ir group" {
@@ -218,8 +248,9 @@ test "ir group" {
             Insn{ .sum = {} },
             Insn{ .push_integer = .{ .value = 3 } },
             Insn{ .product = {} },
+            Insn{ .yield = {} },
         };
-        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
     }
     { // ungrouped
         var ctx = try testSetup("1 + 2 * 3");
@@ -231,8 +262,9 @@ test "ir group" {
             Insn{ .push_integer = .{ .value = 3 } },
             Insn{ .product = {} },
             Insn{ .sum = {} },
+            Insn{ .yield = {} },
         };
-        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
     }
 }
 
@@ -242,8 +274,9 @@ test "declare variable" {
 
     var expected = [_]Insn{
         Insn{ .decl_var = .{ .symbol = 0 } },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
 
 test "assignment" {
@@ -255,8 +288,9 @@ test "assignment" {
             Insn{ .push_symbol = .{ .value = 0 } },
             Insn{ .push_integer = .{ .value = 1 } },
             Insn{ .assign = {} },
+            Insn{ .yield = {} },
         };
-        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
     }
     { // declare and assign
         var ctx = try testSetup("var a = 1");
@@ -266,8 +300,9 @@ test "assignment" {
             Insn{ .decl_var = .{ .symbol = 0 } }, // does decl_var also push the symbol onto the stack?
             Insn{ .push_integer = .{ .value = 1 } },
             Insn{ .assign = {} },
+            Insn{ .yield = {} },
         };
-        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
     }
     {
         var ctx = try testSetup("a = b+c*9");
@@ -281,8 +316,9 @@ test "assignment" {
             Insn{ .product = {} },
             Insn{ .sum = {} },
             Insn{ .assign = {} },
+            Insn{ .yield = {} },
         };
-        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+        try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
     }
 }
 test "declare function" {
@@ -302,8 +338,10 @@ test "declare function" {
         Insn{ .push_symbol = .{ .value = 1 } },
         Insn{ .push_integer = .{ .value = 3 } },
         Insn{ .product = {} },
+        Insn{ .yield = {} },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
 
 test "call function" {
@@ -313,6 +351,7 @@ test "call function" {
     var expected = [_]Insn{
         Insn{ .push_symbol = .{ .value = 0 } },
         Insn{ .call = {} },
+        Insn{ .yield = {} },
     };
-    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir.items);
+    try testing.expectEqualSlices(Insn, expected[0..], ctx.ir);
 }
