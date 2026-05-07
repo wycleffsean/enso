6c453ac3ac438ed5329d7c49665bfee72b5da11f
Sean Carey <wycleffsean@gmail.com>
2026-05-07 00:35:30 -0400

    zig(0.16.0): update flake to pull zig 16

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Enso development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self , flake-utils, nixpkgs ,... }:
