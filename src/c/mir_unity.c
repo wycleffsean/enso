ef4091d3d2eb7cbd1b97d803212581d3c5529a51
Sean Carey <wycleffsean@gmail.com>
2026-07-29 11:54:07 -0400

    mir: fix compilation for macos

--- src/c/mir_unity.c
+++ src/c/mir_unity.c
@@ -1,9 +1,16 @@
 #define _GNU_SOURCE 1
 #define _POSIX_C_SOURCE 200809L
+#ifdef __APPLE__
+#define _DARWIN_C_SOURCE 1
+#endif
 
 #include <string.h>
 #include <stdarg.h>
 #include <setjmp.h>
+
+#ifdef __APPLE__
+#include <alloca.h>
+#endif
 
 #define MIR_x86_64 1
 
