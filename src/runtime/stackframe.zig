ae5bc54257c621d4270d5dc4f5a0c5209eb65140
Sean Carey <wycleffsean@gmail.com>
2026-08-12 00:33:00 -0400

    runtime: store and retrieve values within frame

--- src/runtime/stackframe.zig
+++ src/runtime/stackframe.zig
@@ -4,14 +4,25 @@ const sys = @import("lib/sys.zig");
 const testing = std.testing;
 const test_utils = @import("../test/utils.zig");
 
-const EnsoFrame = extern struct {
+const TaggedValue = struct {};
+
+const EnsoFrame = struct {
     prev: ?*EnsoFrame,
     co: *const CodeObject,
     stack_depth: u16 = undefined,
-    nlocals: u16 = undefined,
+    locals: []TaggedValue,
+
+    fn setLocal(self: *EnsoFrame, index: usize, value: TaggedValue) void {
+        self.locals[index] = value;
+    }
+
+    fn getLocal(self: *EnsoFrame, index: usize) TaggedValue {
+        return self.locals[index];
+    }
 };
 
 pub const StackFrame = struct {
+    allocator: std.mem.Allocator,
     // AFAICT - zig's memorypool internals will work well with a LIFO use case
     pool: std.heap.memory_pool.Extra(EnsoFrame, .{
         .growable = false,
@@ -22,14 +33,15 @@ pub const StackFrame = struct {
 
     const Error = error{
         OverflowError,
-    };
+    } || std.mem.Allocator.Error;
 
     /// capacity - setting to null will use the value from sys.getrecursionlimit()
     ///   any other value is really only appropriate for testing
-    pub fn init(base_allocator: std.mem.Allocator, capacity: ?usize) std.mem.Allocator.Error!Self {
+    pub fn init(allocator: std.mem.Allocator, capacity: ?usize) std.mem.Allocator.Error!Self {
         const cap = capacity orelse sys.getrecursionlimit();
         return .{
-            .pool = try .initCapacity(base_allocator, cap),
+            .allocator = allocator,
+            .pool = try .initCapacity(allocator, cap),
         };
     }
 
@@ -44,7 +56,7 @@ pub const StackFrame = struct {
         };
         const stack_depth = if (self.prev) |prev| prev.stack_depth + 1 else 0;
         frame.co = co;
-        frame.nlocals = nlocals;
+        frame.locals = try self.allocator.alloc(TaggedValue, nlocals);
         frame.stack_depth = stack_depth;
         self.prev = frame;
         return frame;
@@ -53,6 +65,7 @@ pub const StackFrame = struct {
     pub fn pop(self: *Self) void {
         const frame = self.prev.?;
         self.prev = frame.prev;
+        self.allocator.free(frame.locals);
         self.pool.destroy(frame);
     }
 };
@@ -66,4 +79,19 @@ test "overflow" {
 
     _ = try sf.push(&co, 0);
     try testing.expectError(StackFrame.Error.OverflowError, sf.push(&co, 0));
+}
+
+test "set/get local values" {
+    const harness = try test_utils.CompilerHarness.create(testing.allocator);
+    defer harness.deinit();
+    var sf: StackFrame = try .init(testing.allocator, 1);
+    defer sf.deinit(testing.allocator);
+    const co = try harness.buildCodeObjects("");
+
+    const frame = try sf.push(&co, 1000);
+    defer sf.pop();
+    frame.setLocal(0, .{});
+    frame.setLocal(999, .{});
+    _ = frame.getLocal(0);
+    _ = frame.getLocal(999);
 }
