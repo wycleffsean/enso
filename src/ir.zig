dce6c7e335454240b3208065991c6abd46e18d69
Sean Carey <wycleffsean@gmail.com>
2026-08-12 20:18:26 -0400

    ir: calculate a block's successors

--- src/ir.zig
+++ src/ir.zig
@@ -6,6 +6,11 @@ const OpCode = enum(u8) {
     nop,
     identity,
     branch,
+    ret,
+
+    inline fn isTerminator(op: OpCode) bool {
+        return effectsOf(op).terminator;
+    }
 };
 
 const Effects = packed struct(u8) {
@@ -16,11 +21,13 @@ const Effects = packed struct(u8) {
     can_allocate: bool = false,
     terminator: bool = false,
     has_result: bool = false,
+    _padding: i1 = 0,
 };
 
 fn effectsOf(op: OpCode) Effects {
     return switch (op) {
-        .nop => .{},
+        .nop, .identity => .{},
+        .branch, .ret => .{ .terminator = true },
     };
 }
 
@@ -82,11 +89,6 @@ const Value = struct {
     };
 };
 
-const Block = struct {
-    // values: std.ArrayList(ValueId) = .empty, - a span into the procedure's values would be better
-    preds: std.ArrayList(BlockId) = .empty,
-};
-
 const BranchPayload = struct {
     const Extra = struct {
         then: BlockId,
@@ -97,24 +99,44 @@ const BranchPayload = struct {
     extra: Extra,
 };
 
+const Block = struct {
+    // TODO: would a span into the procedure's values would be better?
+    values: std.ArrayList(ValueId) = .empty,
+    preds: std.ArrayList(BlockId) = .empty,
+
+    fn deinit(b: *Block, allocator: std.mem.Allocator) void {
+        b.preds.deinit(allocator);
+        b.values.deinit(allocator);
+    }
+
+    fn terminator(b: *const Block, p: *const Procedure) ValueId {
+        if (b.values.items.len == 0) return .none;
+        const last = b.values.items[b.values.items.len - 1];
+        return if (p.opcodeOf(last).isTerminator()) last else .none;
+    }
+};
+
 /// like in B3! :)
 const Procedure = struct {
     allocator: std.mem.Allocator,
     values: std.MultiArrayList(Value) = .empty,
+    blocks: std.ArrayList(Block) = .empty,
     extra: std.ArrayList(u32) = .empty,
 
     fn deinit(p: *Procedure) void {
         p.values.deinit(p.allocator);
         p.extra.deinit(p.allocator);
+
+        for (p.blocks.items) |*block| block.deinit(p.allocator);
+        p.blocks.deinit(p.allocator);
     }
 
-    fn addValue(p: *Procedure, value: Value) !ValueId {
-        const i = ValueId.from(@intCast(p.values.len));
-        try p.values.append(p.allocator, value);
-        return i;
+    fn opcodeOf(p: *const Procedure, vid: ValueId) OpCode {
+        return p.getValue(vid).op;
     }
-    fn getValue(p: *const Procedure, valueid: ValueId) Value {
-        return p.values.get(valueid.idx());
+
+    fn getValue(p: *const Procedure, vid: ValueId) Value {
+        return p.values.get(vid.idx());
     }
 
     /// Extra Payloads - borrowing this DoD from Zig's own compiler
@@ -148,14 +170,44 @@ const Procedure = struct {
         return out;
     }
 
-    pub fn addBranch(p: *Procedure, payload: BranchPayload) !ValueId {
+    pub fn addBlock(p: *Procedure) !BlockId {
+        const id = BlockId.from(@intCast(p.blocks.items.len));
+        try p.blocks.append(p.allocator, .{});
+        return id;
+    }
+
+    fn addValue(p: *Procedure, bid: BlockId, value: Value) !ValueId {
+        const i = ValueId.from(@intCast(p.values.len));
+        try p.values.append(p.allocator, value);
+        var block = &p.blocks.items[bid.idx()];
+        try block.values.append(p.allocator, i);
+        return i;
+    }
+
+    pub fn addBranch(p: *Procedure, bid: BlockId, payload: BranchPayload) !ValueId {
         const extra_offset = try p.addExtra(BranchPayload.Extra, payload.extra);
-        return try p.addValue(.{
+        return try p.addValue(bid, .{
             .op = .branch,
             .repr = .i1,
             .lhs = payload.predicate,
             .rhs = extra_offset,
         });
+    }
+
+    pub fn successors(p: *const Procedure, bid: BlockId, buf: *[8]BlockId) []const BlockId {
+        const term = p.blocks.items[bid.idx()].terminator(p);
+        if (term == .none) return buf[0..0];
+        // const lhs = p.values.items(.lhs)[term.idx()];
+        const rhs = p.values.items(.rhs)[term.idx()];
+        switch (p.opcodeOf(term)) {
+            .branch => {
+                const extra = p.extraData(BranchPayload.Extra, rhs);
+                buf[0] = extra.then;
+                buf[1] = extra.@"else";
+                return buf[0..2];
+            },
+            else => return buf[0..0],
+        }
     }
 };
 
@@ -166,16 +218,34 @@ test "procedure: encoding 'extra' data" {
     defer proc.deinit();
 
     const predicate: Value = .true;
-    const predicate_id = try proc.addValue(predicate);
-    const then_b: BlockId = .from(0);
-    const else_b: BlockId = .from(1);
+    const root = try proc.addBlock();
+    const then_b = try proc.addBlock();
+    const else_b = try proc.addBlock();
+    const predicate_id = try proc.addValue(root, predicate);
     const branch_extra: BranchPayload.Extra = .{ .then = then_b, .@"else" = else_b };
 
     try testing.expectEqual(0, proc.extra.items.len);
 
-    const vid = try proc.addBranch(.{ .predicate = predicate_id.idx(), .extra = branch_extra });
+    const vid = try proc.addBranch(root, .{ .predicate = predicate_id.idx(), .extra = branch_extra });
     const branch_value = proc.getValue(vid);
 
     const payload = proc.extraData(BranchPayload.Extra, branch_value.rhs);
     try testing.expectEqual(branch_extra, payload);
+}
+
+test "procedure: block successors" {
+    var proc: Procedure = .{
+        .allocator = testing.allocator,
+    };
+    defer proc.deinit();
+
+    const root = try proc.addBlock();
+    const then_b = try proc.addBlock();
+    const else_b = try proc.addBlock();
+
+    const pred = try proc.addValue(root, .true);
+    _ = try proc.addBranch(root, .{ .predicate = pred.idx(), .extra = .{ .then = then_b, .@"else" = else_b } });
+
+    var buf: [8]BlockId = undefined;
+    try testing.expectEqualSlices(BlockId, ([2]BlockId{ then_b, else_b })[0..], proc.successors(root, &buf));
 }
